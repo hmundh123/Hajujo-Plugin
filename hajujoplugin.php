e8c87259720693e315589a88ff71067b520980e9
hmundh123 <harinder.mundh@gmail.com>
2016-03-23 23:56:25 -0400

    Added files via upload
    First set of code kept giving me a 404 page error when creating a new custom post. After many hours or trying to troubleshoot, I read forums and determined it was due to a permalink settings error. I needed to change permalink settings to: /%post_id%/%postname%/
    
    Custom post type has been added - daily food specials
    
    Custom post type posts are showing up on widget - maximum of seven can be chosen (1 for everyday of the week)

--- hajujoplugin.php
+++ hajujoplugin.php
@@ -12,7 +12,7 @@
 
 
 /**
- * Function for Custom Post Type - Hajujo's Food Specials .
+ * Function for Custom Post Type - Daily Food Specials .
  */
 
 function food_specials_custompost() {
@@ -43,100 +43,89 @@ class HajujoWidget extends WP_Widget {
 			'description' => __( 'A display of daily posts from a custom post type.', 'hajujo') 
 	);
 		// Adds a class to the widget and provides a description on the Widget page to describe what the widget does.
-		parent::__construct('hajujo_custom_post', __('Hajujo Custom Post Type', 'hajujo'), $widget_ops);
+		parent::__construct('hajujo_custom_post', __('Daily Food Specials', 'hajujo'), $widget_ops);
+	}
+
+	public function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['numberOfSpecialsPosts'] = strip_tags($new_instance['numberOfSpecialsPosts']);
+		return $instance;
+	}	
+	
+	public function form($instance) {
+		if( $instance) {
+			$title = esc_attr($instance['title']);
+			$numberOfSpecialsPosts = esc_attr($instance['numberOfSpecialsPosts']);
+		} 
+		
+		else {
+			$title = '';
+			$numberOfSpecialsPosts = '';
+	}
+	?>
+		<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'hajujo_widget'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id('numberOfSpecialsPosts'); ?>"><?php _e('Number of Specials Posts:', 'hajujo_widget'); ?></label>
+		<select id="<?php echo $this->get_field_id('numberOfSpecialsPosts'); ?>"  name="<?php echo $this->get_field_name('numberOfSpecialsPosts'); ?>">
+			<?php for($x=1;$x<=7;$x++): ?>
+			<option <?php echo $x == $numberOfSpecialsPosts ? 'selected="selected"' : '';?> value="<?php echo $x;?>"><?php echo $x; ?></option>
+			<?php endfor;?>
+		</select>
+		</p>
+	<?php
+	
 	}
 	
+	public function widget($args, $instance) {
+		
+		extract( $args );
+		$title = apply_filters('widget_title', $instance['title']);
+		$numberOfSpecialsPosts = $instance['numberOfSpecialsPosts'];
+		
+		echo $args['before_widget'];
 	
-	// Determines what will appear on the site
-	public function widget( $args, $instance ) {
-		$c = ! empty( $instance['count'] ) ? '1' : '0'; 
-		//sets a variable for whether or not the 'Count' option is checked
-		$d = ! empty( $instance['dropdown'] ) ? '1' : '0';
-		// sets a variable for whether or not the 'Dropdown' option is checked
-		$title = apply_filters('widget_title', empty($instance['title']) ? __('Hajujo Custom Post Type', 'hajujo') : $instance['title'], $instance, $this->id_base); 
-		// Determines if there's a user-provided title and if not, displays a default title.
-		
-		echo $args['before_widget']; // what's set up when you registered the sidebar
-		
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-
-	if ( $d ) {
-		//if the dropdown option is checked, gets a list of the archives and displays them by year in a dropdown list. 
-		$dropdown_id = "{$this->id_base}-dropdown-{$this->number}";
-?>
-		<label class="screen-reader-text" for="<?php echo esc_attr( $dropdown_id ); ?>"><?php echo $title; ?></label>
-		<select id="<?php echo esc_attr( $dropdown_id ); ?>" name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'>
+	
+		$this->getSpecialsPosts($numberOfSpecialsPosts);
+	
+		echo $after_widget;
+	}
+	
+	public function getSpecialsPosts($numberOfSpecialsPosts) { 
+	
+		global $post;
+		add_image_size( 'hajujo_widget_size', 80, 40, false );
+		$specials = new WP_Query();
+		$specials->query('post_type=food_specials & posts_per_page=' . $numberOfSpecialsPosts );
+		
+		if($specials->found_posts > 0) {
+				echo '<ul class="hajujo_widget">';
+			while ($specials->have_posts()) {
+				$specials->the_post();
+				$image = (has_post_thumbnail($post->ID)) ? get_the_post_thumbnail($post->ID, 'hajujo_widget_size') : '<div class="noThumb"></div>';
+				$dailyspecial = '<li>' . $image;
+				$dailyspecial .= '<a href="' . get_permalink() . '">';
+				$dailyspecial .= get_the_title() . '</a>';
+				echo $dailyspecial;
+			}
 			
-		<?php	$dropdown_args = apply_filters( 'widget_archives_dropdown_args', array(
-				'type'            => 'daily',
-				'format'          => 'option',
-				'show_post_count' => $c // If post count checked, show the post count
-			) );
-		?>	
-			<option value="<?php echo __( 'Select Year', 'hajujo' ); ?>"><?php echo __( 'Select Year', 'hajujo' ); ?></option>
-			<?php wp_get_archives( $dropdown_args ); ?>
-		</select>
-<?php
-	} else {
-		// If (d) not selected, show this:
-?>
-		<ul>
-		<?php 
-			wp_get_archives( apply_filters( 'widget_archives_args', array(
-			'type'            => 'daily',
-			'show_post_count' => $c
-		) ) ); 
-			// gets a list of the archives and displays them by year. If the Count option is checked, this gets shown.
-		?>
-		</ul>
-
-<?php
+			echo '</ul>';
+		
+			wp_reset_postdata();
 		}
 		
-		echo $args['after_widget']; // what's set up when you registered the sidebar
+		else{
+		
+		echo '<p style="padding:10px;">No Specials This Week</p>';
 	}
+}
 	
-	
-	
-	
-	// Sets up the form for users to set their options/add content in the widget admin page
-	
-	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'count' => 0, 'dropdown' => '') );
-		$title = strip_tags($instance['title']);
-		$count = $instance['count'] ? 'checked="checked"' : '';
-		$dropdown = $instance['dropdown'] ? 'checked="checked"' : '';
-?>
-		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'hajujo'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-		</p>
-		<p>
-			<input class="checkbox" type="checkbox" <?php echo $dropdown; ?> id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>" /> 
-			<label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e('Display as dropdown', 'hajujo'); ?></label>
-			<br/>
-			<input class="checkbox" type="checkbox" <?php echo $count; ?> id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" /> 
-			<label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Show post count', 'hajujo'); ?></label>
-		</p>
-<?php }
-
-
-
-
-	
-	// Sanitizes, saves and submits the user-generated content.
-	
-	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$new_instance = wp_parse_args( (array) $new_instance, array( 'title' => '', 'count' => 0, 'dropdown' => '') );
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['count'] = $new_instance['count'] ? 1 : 0;
-		$instance['dropdown'] = $new_instance['dropdown'] ? 1 : 0;
-
-		return $instance;
-	}
 }
 
 // Tells WordPress that this widget has been created and that it should display in the list of available widgets.
