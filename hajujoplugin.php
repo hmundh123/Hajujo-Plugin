a580dde6291af5314cf1ecc95a89d942cc93d49c
hmundh123 <harinder.mundh@gmail.com>
2016-03-24 12:22:12 -0400

    Added files via upload
    Updated files for the addition of a custom post type / widget to display a set number of the custom posts (1-7)
    
    Had minor issues when I went from Localhost to my live website on Phoenix server. This is the final code that worked.

--- hajujoplugin.php
+++ hajujoplugin.php
@@ -48,11 +48,13 @@ class HajujoWidget extends WP_Widget {
 	public function __construct() {
 		$widget_ops = array(
 			'classname' => 'hajujo_widget_specials_posts', 
-			'description' => __( 'A display of daily posts from a custom post type.', 'hajujo') 
+			'description' => __( 'A display of daily food specials that can be added from custom post type - Daily Food Specials.', 'hajujo') 
 	);
-		// Adds a class to the widget and provides a description on the Widget page to describe what the widget does.
+		// Adds a class to the widget and provides a description on the Widget admin page that describes what the Hajujo Daily Food Specials widget does.
 		parent::__construct('hajujo_custom_post', __('Daily Food Specials', 'hajujo'), $widget_ops);
 	}
+
+	// Saves and inputs the user-generated widget content options - the title of the widget and the amount of custom post types that will be displayed on the widget.
 
 	public function update($new_instance, $old_instance) {
 		$instance = $old_instance;
@@ -61,8 +63,10 @@ class HajujoWidget extends WP_Widget {
 		return $instance;
 	}	
 	
+	// This function creates the actual form that our client will see when they access their Wordpress Admin - Widgets section. Our option is to enter a title and the amount of posts the user would like to display as output on their sidebar widget (Option is 1 to 7).
+	
 	public function form($instance) {
-		if( $instance) {
+		if($instance) {
 			$title = esc_attr($instance['title']);
 			$numberOfSpecialsPosts = esc_attr($instance['numberOfSpecialsPosts']);
 		} 
@@ -71,7 +75,7 @@ class HajujoWidget extends WP_Widget {
 			$title = '';
 			$numberOfSpecialsPosts = '';
 	}
-	?>
+	?>       
 		<p>
 		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'hajujo_widget'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
@@ -87,6 +91,8 @@ class HajujoWidget extends WP_Widget {
 	<?php
 	
 	}
+	
+	// Function that determines what will appear on the website - widget output. 
 	
 	public function widget($args, $instance) {
 		
@@ -105,16 +111,16 @@ class HajujoWidget extends WP_Widget {
 		echo $after_widget;
 	}
 	
-	// Function to pull our custom post type posts (Daily Food Specials) that will show on the sidebar widget
+	// Function to query and pull our custom post type posts (Daily Food Specials) that will show on the sidebar widget. The amount of posts dipslayed will be determined by the client. Option is between 1-7 (1 for everyday's special).
 	
 	public function getSpecialsPosts($numberOfSpecialsPosts) { 
 	
 		global $post;
-		add_image_size( 'hajujo_widget_size', 100, 60, false );
+		add_image_size( 'hajujo_widget_size', 140, 100, false );
 		$specials = new WP_Query();
 		$specials->query('post_type=food_specials & posts_per_page=' . $numberOfSpecialsPosts );
 		
-		// If/Else statement to determine what to display if there are posts, and if there are no posts, it will show "No Specials This Week"
+		// If/Else statement to determine what to display if there are posts, and if there are no posts, it will show "No Specials This Week". If there is no thumbnail associated with a post, it will show a 100*60 blank grey box.
 		
 		if($specials->found_posts > 0) {
 				echo '<ul class="hajujo_widget">';
@@ -134,13 +140,15 @@ class HajujoWidget extends WP_Widget {
 		
 		else{
 		
+		// If there are no Daily Food Specials posts (custom post type), "No Specials This Week" will display. 
+		
 		echo '<p style="padding:10px;">No Specials This Week</p>';
 	}
 }
 	
 }
 
-// Tells WordPress that this widget has been created and that it should display in the list of available widgets.
+// Tells WordPress that our Hajujo - Daily Food Specials widget has been created and that it should display in the list of available widgets in the Wordpress admin page.
 
 add_action( 'widgets_init', function(){
      register_widget( 'HajujoWidget' );
